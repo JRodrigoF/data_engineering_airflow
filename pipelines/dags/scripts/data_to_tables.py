d4bbe47a401504b6fa38ee249479fa879ce4e23c
rabauti <rabauti@gmail.com>
2021-12-31 17:25:13 +0200

    scripts

---
+++ pipelines/dags/scripts/data_to_tables.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import sys
+import argparse
+import csv
+
+"""
+Put preprocessed data in tsv for ingestion
+"""
+
+# parsing command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", "-f", type=str, required=True)
+args = parser.parse_args()
+
+filename = args.file
+output_memes = "/opt/airflow/dags/data/memes.tsv"
+output_tags = "/opt/airflow/dags/data/meme_tags.tsv"
+output_types = "/opt/airflow/dags/data/meme_details_types.tsv"
+output_examples = "/opt/airflow/dags/data/meme_content_examples.tsv"
+output_children = "/opt/airflow/dags/data/parent_children_relations.tsv"
+output_siblings = "/opt/airflow/dags/data/siblings_relations.tsv"
+
+if not filename:
+    sys.stderr.write("Error! No input file specified.\n")
+
+sys.stderr.write(f"Read from {filename}.\n")
+
+# read all entries from file
+with open(filename,'r') as f:
+    memes_dict = json.load(f)
+
+#https://stackoverflow.com/questions/60794316/print-get-full-path-of-every-nested-item-in-a-dictionary-python3
+memes_rows = []
+
+# single-value keys
+memes_sv_keys = []
+
+# multi-dimensional keys
+memes_mv_keys = []
+
+meme_tag_rows = []
+meme_details_type_rows = []
+meme_content_example_rows = []
+
+meme_children_rows = []
+meme_siblings_rows = []
+meme_siblings_data = {}
+
+for d in memes_dict:
+    memes_row = []
+    meme = memes_dict[d]
+    memeID = meme['Id']
+
+    if not len(memes_sv_keys):
+        all_keys_properties = meme.keys()
+        for key in sorted(all_keys_properties):
+            if not key.startswith('_'):
+                memes_sv_keys.append(key) # key -> unique value
+            else:
+                memes_mv_keys.append(key) # key -> dict / list of values
+
+        # let's keep in separate single value keys and multi value keys
+        # memes_rows.append(memes_keys + ['total' + key for key in memes_mv_keys])
+        memes_rows.append(memes_sv_keys) # row of columns names
+
+    for key in memes_sv_keys: # grows the row for unique value keys
+        memes_row.append(str(meme[key]))
+
+    # for key in memes_mv_keys:
+    #     memes_row.append(str(len(meme[key])))
+    memes_rows.append(memes_row)
+
+    # tags data
+    for value in meme['_tags']:
+        if not len(meme_tag_rows):
+            meme_tag_rows.append(['key_KYM', 'tag'])
+        meme_tag_rows.append([memeID, value])
+
+    # children data
+    if not len(meme_children_rows):
+        meme_children_rows.append(['memeId', 'child'])
+    elif meme['parent'] != None:
+        meme_children_rows.append([meme['parent'], memeID])
+
+    # siblings data collector
+    if meme['parent'] not in meme_siblings_data:
+        meme_siblings_data[meme['parent']] = [memeID]
+    else:
+        meme_siblings_data[meme['parent']].append(memeID)
+
+    #_details_type
+    for value in meme['_details_type']:
+        if not len(meme_details_type_rows):
+            meme_details_type_rows.append(['key_KYM', 'type'])
+        meme_details_type_rows.append([memeID, value])
+
+    # #meme_keywords_rows
+    # for value in meme['_search_keywords']:
+    #     if not len(meme_keywords_rows):
+    #         meme_keywords_rows.append(['memeId', 'keyword'])
+    #     meme_keywords_rows.append([memeID, value])
+
+    #meme_content_rows
+    for c in meme['_content']:
+        content = meme['_content'][c]
+        # title = content['title']
+        if not len(meme_content_example_rows):
+            meme_content_example_rows.append(['key_KYM', 'variation'])
+            # meme_content_texts_rows.append(['memeId', 'content_title', 'text'])
+            # meme_content_links_rows.append(['memeId', 'content_title', 'url_title', 'url'])
+            # meme_content_images_rows.append(['memeId', 'content_title', 'image'])
+
+        if c == '_examples':
+            for example in content:
+                meme_content_example_rows.append([memeID, example])
+
+        # for text in content['_texts']:
+        #     meme_content_texts_rows.append([memeID, title, text])
+
+        # for link in content['_links']:
+        #     meme_content_links_rows.append([memeID, title, link['title'], link['url']])
+
+        # for img in content['_images']:
+        #     meme_content_images_rows.append([memeID, title, img])
+
+# siblings rows
+for meme in meme_siblings_data:
+    if len(meme_siblings_data[meme]) > 1:
+        head_of_siblings = meme_siblings_data[meme].pop()
+
+        for sibling in meme_siblings_data[meme]:
+            if not len(meme_siblings_rows):
+                meme_siblings_rows.append(['memeID', 'sibling'])
+            meme_siblings_rows.append([head_of_siblings, sibling])
+
+import os
+try:
+    os.stat('tsv')
+except:
+    os.mkdir('tsv')
+
+f = open(output_memes, 'w', encoding='UTF8')
+writer = csv.writer(f,  delimiter = "\t")
+for r in memes_rows:
+    writer.writerow(r)
+f.close()
+
+f = open(output_tags, 'w', encoding='UTF8')
+writer = csv.writer(f,  delimiter = "\t")
+for r in meme_tag_rows:
+    writer.writerow(r)
+f.close()
+
+# f = open('tsv/meme_search_keywords.tsv', 'w', encoding='UTF8')
+# writer = csv.writer(f,  delimiter = "\t")
+# for r in meme_keywords_rows:
+#     writer.writerow(r)
+# f.close()
+
+f = open(output_types, 'w', encoding='UTF8')
+writer = csv.writer(f,  delimiter = "\t")
+for r in meme_details_type_rows:
+    writer.writerow(r)
+f.close()
+
+# f = open('tsv/meme_content_texts_rows.tsv', 'w', encoding='UTF8')
+# writer = csv.writer(f,  delimiter = "\t")
+# for r in meme_content_texts_rows:
+#     writer.writerow(r)
+# f.close()
+
+# f = open('tsv/meme_content_links_rows.tsv', 'w', encoding='UTF8')
+# writer = csv.writer(f,  delimiter = "\t")
+# for r in meme_content_links_rows:
+#     writer.writerow(r)
+# f.close()
+
+# f = open('tsv/meme_content_images_rows.tsv', 'w', encoding='UTF8')
+# writer = csv.writer(f,  delimiter = "\t")
+# for r in meme_content_images_rows:
+#     writer.writerow(r)
+# f.close()
+
+f = open(output_examples, 'w', encoding='UTF8')
+writer = csv.writer(f,  delimiter = "\t")
+for r in meme_content_example_rows:
+    writer.writerow(r)
+f.close()
+
+f = open(output_children, 'w', encoding='UTF8')
+writer = csv.writer(f,  delimiter = "\t")
+for r in meme_children_rows:
+    writer.writerow(r)
+f.close()
+
+f = open(output_siblings, 'w', encoding='UTF8')
+writer = csv.writer(f,  delimiter = "\t")
+for r in meme_siblings_rows:
+    writer.writerow(r)
+f.close()
---
+++ pipelines/dags/scripts/memes_fact_table_maker.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+import argparse
+import pandas as pd
+import numpy as np
+
+"""
+Creates Memes Fact table
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--folder", "-f", type=str, required=True)
+parser.add_argument("--out", "-o", type=str, required=True)
+args = parser.parse_args()
+
+dims_folder = args.folder
+output = args.out
+
+df_memes = pd.read_csv(dims_folder + "memes.tsv", sep="\t")
+df_children = pd.read_csv(
+    dims_folder + "parent_children_relations.tsv", sep="\t")
+df_safeness = pd.read_csv(dims_folder + "meme_safeness_relations.tsv", sep="\t")
+df_tags = pd.read_csv(dims_folder + "meme_tags.tsv", sep="\t")
+df_examples = pd.read_csv(dims_folder + "meme_content_examples.tsv", sep="\t")
+
+dim_dates = pd.read_csv(dims_folder + "dim_dates.tsv", sep="\t")
+dim_status = pd.read_csv(dims_folder + "dim_status.tsv", sep="\t")
+dim_parents = pd.read_csv(dims_folder + "dim_parents.tsv", sep="\t")
+dim_origins = pd.read_csv(dims_folder + "dim_origins.tsv", sep="\t")
+dim_safeness = pd.read_csv(dims_folder + "dim_safeness.tsv", sep="\t")
+
+status_dict = dim_status.set_index('details_status').to_dict()
+
+df_children = (df_children
+    .assign(memeId=lambda x: x['memeId']
+    .str.replace('memes/', ''))
+)
+
+df_memes = (df_memes
+    .rename(columns={'Id': 'key_KYM'})
+    .drop(['category', 'title', 'meta_description', 'template_image_url',
+        'url', 'details_year'], axis=1)
+
+    # date_added_id
+    .assign(added=df_memes
+        .replace({'None': np.nan})
+        .apply(lambda x: x['added'] if pd.notnull(x['added'])
+        else x['last_update_source'], axis='columns')
+        .astype('int64')
+    )
+    .merge(dim_dates, left_on=['added'], right_on=['timestamp'], how='left')
+    .drop(['added', 'timestamp', 'year', 'month', 'day', 'weekday'], axis=1)
+
+    # date_updated_id
+    .merge(dim_dates, left_on=['last_update_source'], right_on=['timestamp'],
+        how='left')
+    .drop(['last_update_source', 'timestamp', 'year', 'month', 'day',
+        'weekday'], axis=1)
+
+    # dim safeness
+    .join(df_safeness.set_index('key_KYM'), on='key_KYM')
+    .assign(safeness_category=lambda x: x['safeness_category']
+        .replace({np.nan: 'None'})
+        .replace('None', 'undefined')
+    )
+    .drop(['adult', 'spoof', 'medical', 'violence', 'racy'], axis=1)
+    .join(dim_safeness.set_index('safeness_category'), on='safeness_category')
+    .drop(['safeness_category'], axis=1)
+
+    # dim status
+    .assign(status_id=lambda x: x['details_status']
+        .replace(status_dict['status_id'])
+    )
+    .drop(['details_status'], axis=1)
+
+    # dim origin
+    .join(dim_origins.set_index('details_origin'), on='details_origin')
+    .drop(['details_origin'], axis=1)
+
+    # dim parent
+    .assign(has_parent=df_memes
+        .replace({'None': np.nan})
+        .apply(lambda x: 1 if pd.notnull(x['parent']) else 0, axis='columns')
+        .astype('Int32')
+    )
+    .drop(['parent'], axis=1)
+
+    # children
+    .assign(children_total=df_memes
+        .merge(df_children, left_on=['Id'], right_on=['memeId'], how='left')
+        .groupby(['Id'])['child'].count().reset_index()
+        .drop(['Id'], axis=1)
+    )
+
+    # examples/variations
+    .assign(content_variations_total=df_memes
+        .merge(df_examples, left_on=['Id'], right_on=['key_KYM'], how='left')
+        .groupby(['Id'])['variation'].count().reset_index()
+        .drop(['Id'], axis=1)
+    )
+
+    # tags
+    .assign(tags_total=df_memes
+        .merge(df_tags, left_on=['Id'], right_on=['key_KYM'], how='left')
+        .groupby(['Id'])['tag'].count().reset_index()
+        .drop(['Id'], axis=1)
+    )
+
+    .drop(['key_KYM'], axis=1)
+    .rename({'date_id_x': 'date_added_id', 'date_id_y': 'date_updated_id'},
+            axis=1)
+    .sort_values(['date_added_id'])
+    .reset_index(drop=True)
+
+    # Id
+    .assign(Id=lambda x: range(1, len(x) + 1))
+    .filter(items=['Id', 'date_added_id', 'date_updated_id', 'safeness_id',
+                'status_id', 'origin_id', 'has_parent', 'children_total',
+                'content_variations_total', 'tags_total'])
+
+)
+
+df_memes.to_csv(output, sep="\t", encoding='utf-8', na_rep='None', index=False)
+# display(df_memes)
